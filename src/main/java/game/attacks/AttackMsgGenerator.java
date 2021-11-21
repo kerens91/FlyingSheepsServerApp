b121819f52faf346319016a0cb0f026df095618a
Keren <kerensol16@gmail.com>
2021-11-22 01:25:43 +0200

    FlyingSheepsServerApp: updating 'about the game' document

--- src/main/java/game/attacks/AttackMsgGenerator.java
+++ src/main/java/game/attacks/AttackMsgGenerator.java
@@ -262,7 +262,7 @@ public class AttackMsgGenerator {
      * created the Type.titleImg type message
      */
     public AttackMsg getMsgAllNatureAttFail() {
-    	return generateMsgTitleImg(DEST_ALL, attackState.getHelperCard().getImg());
+    	return generateMsgTwoImg(DEST_ALL, attackState.getHelperCard().getImg(), attackState.getAttackCard().getImg());
     }
     
 }
